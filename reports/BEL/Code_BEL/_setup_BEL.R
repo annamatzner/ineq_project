ba2ac46796bc5af2de55c673a7a0bf9b34004fd8
annamatzner <anna.r.matzner@gmail.com>
2018-12-04 14:25:11 +0100

    setup income variables

--- reports/BEL/Code_BEL/_setup_BEL.R
+++ reports/BEL/Code_BEL/_setup_BEL.R
@@ -1,57 +1,135 @@
-# Setup -------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
+# Setup Data Belgium
+# prepare data frame and generate income variables
+
+# -----------------------------------------------------------------------------
 
 library(dplyr)
-#if(!exists(c("country", "year"))) {
-#  stop("Please specify country and year.")
-#}
 
-# Prepare Data ------------------------------------------------------------
+# Prepare Data ----------------------------------------------------------------
 
 # Download data
 silc.p <- tbl(pg, "pp") %>%
-  filter(pb020 == 'BE') %>%
-  select(pb010, pb020, pb030, pb040, pb150, py010g, py050g, px010, px030, 
-         py080g, py010n, py050n, py080n, py140n, py090n, py100n, py110n, 
-         py120n, py130n, hy040g, hy030g ) %>%
+  filter(pb010 == 2013 & pb020 == 'BE') %>%
+  select(pb030, py010g, py050g,
+         py080g, py090g, py100g, py110g, py120g, py130g, py140g, 
+         pl060, pl100, px010) %>%
   collect(n = Inf)
-
+# py021g (!), pl073 & pl074 (für Laeken Indicators) nicht gefunden
 
 silc.h <- tbl(pg, "hh") %>%
-  filter(hb020 == 'BE') %>%
-  select(hb020, hb030, hy010, hx010, hy040g, hy030g, hy170g, hy040n, 
-         hy030n, hy170n,hy020) %>%
+  filter(hb010 == 2013 & hb020 == 'BE') %>%
+  select(hb010, hb020, hb030, hy020, hy030g, hy040g, hy050g, hy060g, hy070g, 
+         hy080g, hy090g, hy110g, hy120g, hy130g, hy140g, hx040, hx050) %>%
   collect(n = Inf)
 
 silc.d <- tbl(pg, "dd") %>%
-  filter(db020 == 'BE') %>%
+  filter(db010 == 2013 & db020 == 'BE') %>%
   select(db010, db020, db030, db040, db090) %>%
   collect(n = Inf)
 
 silc.r <- tbl(pg, "rr") %>% 
-  filter(rb020 == 'BE') %>%
-  select(rb010, rb020, rb030, rb050) %>%
+  filter(rb010 == 2013 & rb020 == 'BE') %>%
+  select(rb010, rb020, rb030, rb050, rb080, rb090, rx030) %>%
   collect(n = Inf)
 
-# Create unique IDs for merging, merge country and household ID
-silc.p <- silc.p %>% mutate(id_h = paste0(pb020, px030))
+# Rename rb030, pb030 to personal_id
+silc.r <- silc.r %>% rename(personal_id = rb030)
+silc.p <- silc.p %>% rename(personal_id = pb030)
+
+# merge silc.r and silc.p
+silc.rp <- left_join(silc.r, silc.p)
+
+# Create age, household ID, gender variables
+silc.rp <- silc.rp %>% 
+  mutate(age = rb010 - rb080,
+         gender = factor(rb090,labels = c('Male','Female')),
+         id_h = paste0(rb020,rx030)) 
+
+# Create unique IDs for merging, merge country and household ID h,d
 
 silc.h <- silc.h %>% mutate(id_h = paste0(hb020, hb030))
 
 silc.d <- silc.d %>% mutate(id_h = paste0(db020, db030))
 
-# Merge the datasets
-silc.pd <- left_join(silc.p, silc.d %>% select(id_h, db020, db090))
+# Merge silc.rp and silc.h
+silc.rph <- left_join(silc.rp, silc.h)
 
-silc.hd <- left_join(silc.h, silc.d)
+# Replace NA's in silc.rph by 0
+silc.rph[is.na(silc.rph)] <- 0
+
+# P1 EUROSTAT -----------------------------------------------------------------
+
+# Pre-tax factor income (Canberra: primary income) ----------------------------
+
+# Sum up personal income
+silc.rph <- silc.rph %>% mutate(pincome1 = py010g + py050g + py080g)
+# company car missing
+
+# Sum up personal income of HH
+silc.rph <- silc.rph %>% group_by(id_h) %>% 
+  do({sum_pincome1 = sum(distinct(.,pincome1)$pincome1); 
+  mutate(., sum_pincome1 = sum_pincome1)})
+
+# Equivalised HH income per person
+silc.rph <- silc.rph %>% 
+  mutate(income_p1_1 = ((sum_pincome1 + hy110g + hy040g + hy090g) / hx050))
+
+# Pre-tax national income -----------------------------------------------------
+
+# Sum up personal income 
+silc.rph <- silc.rph %>% mutate(pincome2 = py090g + py100g)
+
+# Sum up personal income of HH
+silc.rph <- silc.rph %>% group_by(id_h) %>% 
+  do({sum_pincome2 = sum(distinct(.,pincome2)$pincome2); 
+  mutate(., sum_pincome2 = sum_pincome2)})
+
+# Equivalised HH income per person
+silc.rph <- silc.rph %>%
+  mutate(income_p1_2 = (income_p1_1 + sum_pincome2 / hx050))
+
+# Post-tax national income ----------------------------------------------------
+
+# Sum up personal income 
+silc.rph <- silc.rph %>% mutate(pincome3 = py110g + py120g + py130g + py140g)
+
+# Sum up personal income of HH
+silc.rph <- silc.rph %>% group_by(id_h) %>% 
+  do({sum_pincome3 = sum(distinct(.,pincome3)$pincome3); 
+  mutate(., sum_pincome3 = sum_pincome3)})
+
+# Equivalised HH income per person
+silc.rph <- silc.rph %>%
+  mutate(income_p1_3 = (income_p1_2 + 
+                          (sum_pincome3 + hy050g + hy060g + hy070g + hy080g 
+                           - hy120g - hy130g - hy140g) / hx050))
 
 
-# Create total personal income --------------------------------------------
+# P2 WID.WORLD ----------------------------------------------------------------
 
-# Find string "py" (i.e. income variables) for summing up total personal income. 
-silc.pd <- silc.pd %>% 
-  mutate(total.inc = rowSums(silc.pd[, grep("py", colnames(silc.pd))], 
-                             na.rm = TRUE)) 
+# Achtung: Variablen stimmen nur für Personen >= 20 Jahre. 
 
-# Fin ---------------------------------------------------------------------
+# Generate variable ("n"): count of hh members age >= 20 
+silc.rph <- silc.rph %>% 
+  add_count(age >= 20, id_h)
 
-message("Prepared data for ", country, " in ", year, ".")
+# Pre-tax factor income (Canberra: primary income) ----------------------------
+silc.rph <- silc.rph %>%
+  mutate(income_p2_1 = py010g + py050g + py080g + (hy110g + hy040g + hy090g)/n)
+
+# Pre-tax national income -----------------------------------------------------
+silc.rph <- silc.rph %>%
+  mutate(income_p2_2 = income_p2_1 + py090g + py100g)
+
+# Post-tax disposable income --------------------------------------------------
+silc.rph <- silc.rph %>%
+  mutate(income_p2_3 = income_p2_2 + py110g + py120g + py130g + py140g + 
+           (hy050g + hy060g + hy070g + hy080g - hy120g - hy130g - hy140g)/n)
+
+# Fin -------------------------------------------------------------------------
+
+# Um Daten abzuspeichern
+
+write.delim(silc.rph,)
