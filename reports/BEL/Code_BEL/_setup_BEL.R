cb67b60ed01d5c3983eda7beefe2fbdf7f062604
annamatzner <anna.r.matzner@gmail.com>
2018-12-06 21:31:02 +0100

    Variable py021g eingefügt

--- reports/BEL/Code_BEL/_setup_BEL.R
+++ reports/BEL/Code_BEL/_setup_BEL.R
@@ -10,26 +10,24 @@ library(dplyr)
 # Prepare Data ----------------------------------------------------------------
 
 # Download data
-silc.p <- tbl(pg, "pp") %>%
+silc.p <- tbl(pg, "c13p") %>%
   filter(pb010 == 2013 & pb020 == 'BE') %>%
-  select(pb030, py010g, py050g,
-         py080g, py090g, py100g, py110g, py120g, py130g, py140g, 
-         pl060, pl100, px010) %>%
+  select(pb030, py010g, py050g, py021g, py080g, py090g, py100g, py110g, py120g, 
+         py130g, py140g, pl060, pl100, px010) %>%
   collect(n = Inf)
-# py021g (!), pl073 & pl074 (für Laeken Indicators) nicht gefunden
 
-silc.h <- tbl(pg, "hh") %>%
+silc.h <- tbl(pg, "c13h") %>%
   filter(hb010 == 2013 & hb020 == 'BE') %>%
   select(hb010, hb020, hb030, hy020, hy030g, hy040g, hy050g, hy060g, hy070g, 
          hy080g, hy090g, hy110g, hy120g, hy130g, hy140g, hx040, hx050) %>%
   collect(n = Inf)
 
-silc.d <- tbl(pg, "dd") %>%
+silc.d <- tbl(pg, "c13d") %>%
   filter(db010 == 2013 & db020 == 'BE') %>%
   select(db010, db020, db030, db040, db090) %>%
   collect(n = Inf)
 
-silc.r <- tbl(pg, "rr") %>% 
+silc.r <- tbl(pg, "c13r") %>% 
   filter(rb010 == 2013 & rb020 == 'BE') %>%
   select(rb010, rb020, rb030, rb050, rb080, rb090, rx030) %>%
   collect(n = Inf)
@@ -64,8 +62,7 @@ silc.rph[is.na(silc.rph)] <- 0
 # Pre-tax factor income (Canberra: primary income) ----------------------------
 
 # Sum up personal income
-silc.rph <- silc.rph %>% mutate(pincome1 = py010g + py050g + py080g)
-# company car missing
+silc.rph <- silc.rph %>% mutate(pincome1 = py010g + py050g + py080g + py021g)
 
 # Sum up personal income of HH
 silc.rph <- silc.rph %>% group_by(id_h) %>% 
@@ -106,18 +103,20 @@ silc.rph <- silc.rph %>%
                           (sum_pincome3 + hy050g + hy060g + hy070g + hy080g 
                            - hy120g - hy130g - hy140g) / hx050))
 
+silc.rph$hy020/silc.rph$hx050
 
 # P2 WID.WORLD ----------------------------------------------------------------
-
-# Achtung: Variablen stimmen nur für Personen >= 20 Jahre. 
 
 # Generate variable ("n"): count of hh members age >= 20 
 silc.rph <- silc.rph %>% 
   add_count(age >= 20, id_h)
 
+# Achtung: Variablen stimmen nur noch für Personen >= 20 Jahre. 
+
 # Pre-tax factor income (Canberra: primary income) ----------------------------
 silc.rph <- silc.rph %>%
-  mutate(income_p2_1 = py010g + py050g + py080g + (hy110g + hy040g + hy090g)/n)
+  mutate(income_p2_1 = py010g + py021g + py050g + py080g + 
+           (hy110g + hy040g + hy090g)/n)
 
 # Pre-tax national income -----------------------------------------------------
 silc.rph <- silc.rph %>%
@@ -130,6 +129,4 @@ silc.rph <- silc.rph %>%
 
 # Fin -------------------------------------------------------------------------
 
-# Um Daten abzuspeichern
 
-write.delim(silc.rph,)
